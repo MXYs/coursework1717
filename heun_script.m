02e4bf0477ef0546c5487f2ade2667862ef804bb
MXYs <chellemah@gmail.com>
2017-03-10 15:58:10 +0000

    Update heun_script.m

--- heun_script.m
+++ heun_script.m
@@ -1,18 +1,20 @@
-%initialise parameters
+% initialise parameters and set up initial conditions
 R = 0.5; L = 0.0015; t0 = 0; iL0 = 0; tf = 0.05; 
-T1 = 25e-6; T2 = 140e-6; T3 = 550e-6; T4 = 1200e-6;
+T1 = 25e-6; T2 = 140e-6; T3 = 550e-6; T4 = 1200e-6; % 4 different input period parameters
 tao1 = 140e-6; %tao2=140e-12;
-h = 0.002; %set step size
+h = 0.002; % set step size, changeable when passing in to heun.m
 %--------------------------------------
 %             step input      
 %--------------------------------------
-Vin = @(t) 5; %define input function
+Vin = @(t) 5; % define input function
 [t,vout] = heun(Vin,R,L,t0,iL0,0.04,0.0002);  % call to heun.m
-subplot(3,2,[1,2]);
+subplot(3,2,[1,2]); %arrange plot positions
 plot(t,vout,'b.');  % plot the numerical solution
 hold on;
-plot(t,Vin(t),'r');
-title('vin = step');
+plot(t,Vin(t),'r'); % plot input function on the same plot to give clear comparison betweeen input and output
+title('vin = step'); 
+% all the code pieces below follow the same pattern
+% different tf and h are passed when call heun.m in order to view the whole plot clearly. 
 
 %----------------------------------------
 %        impulsive signal input 
@@ -23,7 +25,7 @@ subplot(3,2,[3,4]);
 plot(t,vout,'b.');
 hold on;
 plot(t,Vin(t),'r');
-title('impulsive signal input');
+title('exponential input');
 %---------------------------------------------
 %          exponential decay                 
 %---------------------------------------------
@@ -33,7 +35,7 @@ subplot(3,2,[5,6]);
 plot(t,vout,'b.');
 hold on;
 plot(t,Vin(t),'r');
-title('exp decay input');
+title('impulsive signal input');
 
 %-----------------------------------------------------
 %           sine wave input 
@@ -109,13 +111,12 @@ plot(t,vout,'b.');
 hold on;
 plot(t,Vin(t),'r');
 title('T=1200us');
-%----------------
-
 
 
 figure
-% sawtooth wave input
-% ---------------------------------------------------------------
+% --------------------------------------------------
+%           sawtooth wave input
+% --------------------------------------------------
 Vin = @(t) 5*sawtooth(2*pi*t/T1);
 [t,vout] = heun(Vin,R,L,t0,iL0,0.0001,0.000002);
 subplot(2,2,1);
@@ -124,7 +125,7 @@ plot(t,vout,'b.');
 hold on;
 plot(t,Vin(t),'r');
 title('T=25us');
-%----
+
 Vin = @(t) 5*sawtooth(2*pi*t/T2);
 [t,vout] = heun(Vin,R,L,t0,iL0,0.00018,0.000002);
 subplot(2,2,2);
@@ -132,7 +133,7 @@ plot(t,vout,'b.');
 hold on;
 plot(t,Vin(t),'r');
 title('T=140us');
-%------
+
 Vin = @(t) 5*sawtooth(2*pi*t/T3);
 [t,vout] = heun(Vin,R,L,t0,iL0,0.0010,0.00001);
 subplot(2,2,3);
@@ -140,7 +141,7 @@ plot(t,vout,'b.');
 hold on;
 plot(t,Vin(t),'r');
 title('T=550us');
-%------
+
 Vin = @(t) 5*sawtooth(2*pi*t/T4);
 [t,vout] = heun(Vin,R,L,t0,iL0,0.0013,0.00001);
 subplot(2,2,4);
@@ -148,8 +149,5 @@ plot(t,vout,'b.');
 hold on;
 plot(t,Vin(t),'r');
 title('T=12000us');
-
-
-
 
 
