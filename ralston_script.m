22aa864642008e6978b18fb211189690bbe0caa6
MXYs <chellemah@gmail.com>
2017-03-10 15:47:38 +0000

    Update ralston_script.m

--- ralston_script.m
+++ ralston_script.m
@@ -1,28 +1,30 @@
-R = 0.5; L = 0.0015; t0 = 0; iL0 = 0;
-T1 = 25e-6; T2 = 140e-6; T3 = 550e-6; T4 = 1200e-6;
+R = 0.5; L = 0.0015; t0 = 0; iL0 = 0;    %set up initial condition and parameters
+T1 = 25e-6; T2 = 140e-6; T3 = 550e-6; T4 = 1200e-6; % period parameter for input
 
 % step input -----------------------------------
-Vin = @(t) 5;
-[t,vout] = ralston(Vin,R,L,t0,iL0,0.05,0.002);
-subplot(2,2,[1,2]);
-plot(t,vout,'b.');
+Vin = @(t) 5;   % set up input function
+[t,vout] = ralston(Vin,R,L,t0,iL0,0.05,0.002); % call to ralston.m
+-subplot(2,2,[1,2]); % arrange plot position
+plot(t,vout,'b.'); % plot vout against time 
 hold on;
-plot(t,Vin(t),'r');
-title('Vout with step input');
+plot(t,Vin(t),'r'); % plot inut on the same plot for easy comparison
+title('Vout with step input'); % name the plot
+% All the code pieces below follow a similiar pattern
+% tf and h passed in to ralston.m are different every time 
+% to give a clear overview of the plot
 
-
-% exponential input ----------------------------
-Vin = @(t) 4*exp(-(t.^2)/0.00014);
-[t,vout] = ralston(Vin,R,L,t0,iL0,0.05,0.002);
+% -------------- exponential input ----------------------------
+-Vin = @(t) 4*exp(-(t.^2)/0.00014); %set up input function
+[t,vout] = ralston(Vin,R,L,t0,iL0,0.05,0.002); % call to ralston.m
 subplot(2,2,3);
 plot(t,vout,'b.');
 hold on;
 plot(t,Vin(t),'r');
 title('for exponential input');
 
-% exp input 2 ---------------------------------
+% ----------------- exp input 2 ---------------------------------
 Vin = @(t) 4*exp(-t/0.00014);
-[t,vout] = ralston(Vin,R,L,t0,iL0,0.05,0.002);
+-[t,vout] = ralston(Vin,R,L,t0,iL0,0.05,0.002); 
 subplot(2,2,4);
 plot(t,vout,'b.');
 hold on;
@@ -30,7 +32,8 @@ plot(t,Vin(t),'r');
 title('for impulsive input');
 
 
-% sine wave input ---------------------------------------
+% ---------------  sine wave input ---------------------------------
+% 4 pieces of code are duplicated to give 4 plots on 4 different input periods:T1,T2,T3 and T4 
 figure
 Vin = @(t) 5*sin(2*pi*t/T1);
 [t,vout] = ralston(Vin,R,L,t0,iL0,0.00014,0.000002);
@@ -45,7 +48,7 @@ Vin = @(t) 5*sin(2*pi*t/T2);
 subplot(2,2,2);
 plot(t,vout,'b.');
 hold on;
-plot(t,Vin(t),'r');
+-plot(t,Vin(t),'r');
 title('T=140us');
 
 Vin = @(t) 5*sin(2*pi*t/T3);
@@ -65,7 +68,7 @@ plot(t,Vin(t),'r');
 title('T=1200us');
 
 
-% square wave input ---------------------------------------
+% ------------- square wave input ------------------------------
 figure
 Vin = @(t) 5*square(2*pi*t/T1);
 [t,vout] = ralston(Vin,R,L,t0,iL0,0.0001,0.000002);
@@ -99,7 +102,7 @@ hold on;
 plot(t,Vin(t),'r');
 title('T=1200us');
 
-% sawtooth wave input ------------------------------------------
+% --------------- sawtooth wave input ------------------------------
 figure
 Vin = @(t) 5*sawtooth(2*pi*t/T1);
 [t,vout] = ralston(Vin,R,L,t0,iL0,0.0001,0.000002);
